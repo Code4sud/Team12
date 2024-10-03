e09b899de8df1337bbcdb9228dfe84b62db8c222
thomasvsl <thomas.vidal-savelli@epitech.eu>
2024-10-03 19:07:13 +0200

    front

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Navbar } from './components/Navbar.tsx'
 import App from './App.tsx'
 import './index.css'
-import Header from './Header/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header />
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
